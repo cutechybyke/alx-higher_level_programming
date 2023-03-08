bd9ac8644c62f2532729a7d8d072802845cd9e8e
Obinna Nwaneri <cutechybyke01@gmail.com>
2023-03-07 19:33:46 -0800

    yo

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for love in range(97, 123):
-        print("{}".format(chr(love)), end="")
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
