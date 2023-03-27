a9a444c9c572281d520287f05cadb35e1bf429fd
obinna nwaneri <cutechybyke01@gmail.com>
2023-03-27 14:08:22 -0700

    calc

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     result = 0
 
@@ -8,8 +9,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
 
-
-                result += a ** b / i
+            result += a ** b / i
         except:
             result = b + a
             break
