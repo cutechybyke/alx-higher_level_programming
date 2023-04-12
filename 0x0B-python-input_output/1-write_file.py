09b592cd9700b56834cfd5a26b571b86ae879f45
Obinna Nwaneri <cutechybyke01@gmail.com>
2023-04-12 13:15:37 -0700

    yess

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,16 @@
-#!/usr/bin/python3
-"""
-This here Contains the function "wrtie_file"
+!/usr/bin/python3
+""" Here is a Module that contains a function  writes to a text file
 """
 
+
 def write_file(filename="", text=""):
-    """this here returns the number of chars written to "filename" from "text" """
-    with open(filename, 'w', encoding='utf=8') as f:
+    """ this is a Function that writes to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raise
+        Exception: when the file can be opened
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
