7b70aa6e781094522fdb693e0f365aeb21fb0ce6
obinna nwaneri <chibuike0binna3@gmail.com>
2023-06-23 11:47:00 -0700

    for thiiss

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module is for retreiving and printing all states froom a MSql database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm inport sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    # Create the SQLAlchemy engine using the  provided MySQL credentials
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Create a session
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Search for the specified state in database
+    louisiana = State(name="Louisiana")
+    # Add the new statete to session
+    session.add(louisiana)
+    # Commit the session to persist the changes
+    session.commit()
+    # Print the ID of the newly added state
+    print(louisiana.id)
+    
